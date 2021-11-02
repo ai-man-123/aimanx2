b1197b506ca88e3414f22fcaa069f00a22fa12f4
ameys <91192362+Xmell91@users.noreply.github.com>
2021-11-02 20:37:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ let { color, bgcolor } = require('./lib/color')
 const cp = require('child_process')
 const simple = require('./lib/simple.js')
 const WAPI = simple.WAConnection(WAConnection)
+const fetch = require('./lib/fetcher')
 let fs = require('fs')
 let os = require('os')
 let figlet = require('figlet')
-let authfile = './session.json'
+global.authfile = './session.json'
 async function mulai() {
 global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
-let melcanz = new WAPI()
-melcanz.browserDescription = Browsers.appropriate('Desktop')
-melcanz.browserDescription[0] = "Bot WhatsApp By @melcanz.io"
-melcanz.logger.level = 'warn'
+let Melcanz = new WAPI()
+Melcanz.browserDescription = Browsers.appropriate('Desktop')
+Melcanz.browserDescription[0] = "Bot WhatsApp By @Melcanz.io"
+var { currentVersion } = await fetch.json(`https://web.whatsapp.com/check-update?version=1&platform=web`)
+Melcanz.version = currentVersion.split('.').map(a => parseInt(a)) || [2, 2140, 12]
+Melcanz.logger.level = 'warn'
 console.log(color(figlet.textSync('Base Wabot', {
 		font: 'Standard',
 		horizontalLayout: 'default',
@@ -26,48 +29,51 @@ console.log(color(figlet.textSync('Base Wabot', {
 		width: 80,
 		whitespaceBreak: false
 	}), 'cyan'))
-console.log(color('[ CREATED BY Melcanz ]'))
-melcanz.on('qr', async () => {
+console.log(color('[ CREATED BY Melcanz.io ]'))
+Melcanz.on('qr', async () => {
 console.log('Scan kode qr ini untuk menjalankan bot')
 })
-fs.existsSync(authfile) && melcanz.loadAuthInfo(authfile)
-	melcanz.on('connecting', () => {
+fs.existsSync(authfile) && Melcanz.loadAuthInfo(authfile)
+	Melcanz.on('connecting', () => {
 		console.log(color('[CLIENT]', 'cyan'), color('Connecting...', 'yellow'))
 	})
-	melcanz.on('close', async ({ reason, isReconnecting }) => {
+	Melcanz.on('close', async ({ reason, isReconnecting }) => {
 	console.log(color('[CLIENT]', 'cyan'), color(`Because ${reason} reconnecting : ${isReconnecting}`, 'yellow'))
 	if (!isReconnecting && reason == 'invalid_session') {
-           console.log(color('[CLIENT]', 'cyan'), color('Sesion Invalid, deleting session', 'yellow'))
+           console.log(color('[CLIENT]', 'cyan'), color('Sesion Invalid, deleting session', 'rex'))
             if (fs.existsSync(authfile)) {
                 fs.unlinkSync(authfile)
             }
-            melcanz.clearAuthInfo()
+            Melcanz.clearAuthInfo()
 	    mulai()
         }
 	})
-	melcanz.on('open', (ye) => {
+	Melcanz.on('open', (ye) => {
 		console.log(color('[CLIENT]', 'cyan'), color('Connected...', 'green'))
 	})
-	await melcanz.connect().then(async v => {
-        global.server ? require('./server')(melcanz) : ''
-	console.log(`Nama Bot : ${melcanz.user.name}\nID Bot : ${awesome('+'+melcanz.user.jid.split('@')[0]).getNumber('international')}\nMode : ${selfmode ? 'Self Mode' : 'Public Mode'}\nHostname : ${os.hostname()}`)
-		fs.writeFileSync(authfile, JSON.stringify(melcanz.base64EncodedAuthInfo(), null, '\t'))
-	owner.map(a => melcanz.reply(a + "@c.us", 'Bot Started.....'))
+	await Melcanz.connect().then(async v => { 
+        console.log(color(`[CLIENT]`, 'cyan'), color('WhatsApp Web Running On Version :'), color(Melcanz.version.join('.'), 'yellow'))
+        global.server ? require('./server')(Melcanz) : ''
+	// console.log(`Nama Bot : ${Melcanz.user.name}\nID Bot : ${awesome('+'+Melcanz.user.jid.split('@')[0]).getNumber('international')}\nMode : ${selfmode ? 'Self Mode' : 'Public Mode'}\nHostname : ${os.hostname()}`)
+		if (!fs.existsSync(authfile)) fs.writeFileSync(authfile, JSON.stringify(Melcanz.base64EncodedAuthInfo(), null, '\t'))
+	owner.map(a => Melcanz.reply(a + "@c.us", 'Bot Started.....'))
 		})
-    melcanz.on('CB:action,,call', id => {
-    require('./message/call')(melcanz, id)
+    Melcanz.on('CB:action,,call', id => {
+    require('./message/call')(Melcanz, id)
     })
-    melcanz.on('group-participants-update', async (anu) => {
+    Melcanz.on('group-participants-update', async (anu) => {
 	console.log(anu)
-	require('./message/detect')(melcanz, anu)
+	require('./message/detect')(Melcanz, anu)
 	})
-	melcanz.on('CB:action,,battery', json => {
-      melcanz.battery = Object.fromEntries(Object.entries(json[2][0][1]).map(v => [v[0], eval(v[1])]))
+	Melcanz.on('CB:action,,battery', json => {
+      Melcanz.battery = Object.fromEntries(Object.entries(json[2][0][1]).map(v => [v[0], eval(v[1])]))
+      console.log(color(`[CLIENT]`, 'cyan'), color('Battery Updated!', 'yellow'))
+      console.log(Melcanz.battery)
       })
-      melcanz.on('message-delete', async (m) => {
-      require('./message/antidelete')(melcanz, m)
+      Melcanz.on('message-delete', async (m) => {
+      require('./message/antidelete')(Melcanz, m)
     })
-    melcanz.on('chat-update', async chatUpdate => {
+    Melcanz.on('chat-update', async chatUpdate => {
     try {
      if (!chatUpdate.hasNewMessage) return   
      if (!chatUpdate.messages && !chatUpdate.count) return
@@ -75,12 +81,12 @@ fs.existsSync(authfile) && melcanz.loadAuthInfo(authfile)
  let msg = chatUpdate.messages.all()[0]
 	 if (!msg.message) return
      msg.message = msg.message.hasOwnProperty('ephemeralMessage') ? msg.message.ephemeralMessage.message : msg.message
-       simple.smsg(melcanz, msg)
+       simple.smsg(Melcanz, msg)
         if (msg.key && msg.key.remoteJid == 'status@broadcast') return 
 	if (!msg.key.fromMe && selfmode) return
 	if (msg.key.id.startsWith('XYZ0')) return
-	if(autoread) melcanz.chatRead(msg.chat)
-    require('./message/melcanz')(melcanz, msg)
+	if(autoread) Melcanz.chatRead(msg.chat)
+    require('./message/Melcanz')(Melcanz, msg)
     } catch (e) {
     console.log(color('[ERR]', 'cyan'), color(e, 'red'))
     }
